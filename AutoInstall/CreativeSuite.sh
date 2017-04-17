0c3d80ec41b5539cb0c46b14ed57c06bd52460f1
JoshuaFlynn <flynn4anything@hotmail.com>
2017-04-17 16:22:29 +0100

    Add files via upload

--- AutoInstall/CreativeSuite.sh
+++ AutoInstall/CreativeSuite.sh
@@ -13,11 +13,21 @@ apt-get install -y kdenlive
 #Linux's answer to powerful graphical editing (GIMP stands for Graphics Image Manipulation Program)
 apt-get install -y gimp
 
+#Good for bulk converting images and the like, resizing etc
+apt-get install imagemagick
+
 #Audio editing and recording
 apt-get install -y audacity
 
 #Linux answer to the paint program
 apt-get install -y kolourpaint4
 
+#Animation tool
 #You can also manually install an up-to-date version of synfig from their site!
 apt-get install -y synfig
+
+#This is no longer required as kolourpaint4 is superior
+apt-get remove -y mtpaint
+
+#Clean up after ourselves
+apt-get autoremove -y
---
+++ AutoInstall/WorkSuite.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y libreoffice
+apt-get install -y evolution
+
+apt-get remove -y sylpheed
+apt-get remove -y gnumeric
+apt-get remove -y abiword
+apt-get remove -y libreoffice-math
+apt-get remove -y libreoffice-draw
+apt-get autoremove -y

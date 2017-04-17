470bab7a2f2f53b97e6622525154539edbc1af0a
JoshuaFlynn <flynn4anything@hotmail.com>
2017-04-17 19:44:22 +0100

    Add files via upload

---
+++ MasterInstall.sh
@@ -0,0 +1,8 @@
+bash AutoInstall/Security.sh
+bash AutoInstall/Browser.sh
+bash AutoInstall/SystemTools.sh
+bash AutoInstall/CreativeSuite.sh
+bash AutoInstall/WorkSuite.sh
+bash AutoInstall/ProgrammingSuite.sh
+bash AutoInstall/EntertainmentSuite.sh
+bash AutoInstall/GamingSuite.sh

161e3d979535ac49b83fda9a01c728f68204a6df
JoshuaFlynn <flynn4anything@hotmail.com>
2017-04-23 12:54:11 +0100

    Add files via upload
    Temporarily removed the remove commands for 'LibreOffice math' and 'draw', as this seems to cause the entire LibreOffice package to be uninstalled.

--- AutoInstall/WorkSuite.sh
+++ AutoInstall/WorkSuite.sh
@@ -5,6 +5,4 @@ apt-get install -y evolution
 apt-get remove -y sylpheed
 apt-get remove -y gnumeric
 apt-get remove -y abiword
-apt-get remove -y libreoffice-math
-apt-get remove -y libreoffice-draw
 apt-get autoremove -y

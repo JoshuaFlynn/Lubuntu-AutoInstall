0ed3ae4251212bed4a83dd070eee50f08a416cac
JoshuaFlynn <flynn4anything@hotmail.com>
2017-04-23 12:55:56 +0100

    Add files via upload
    Corrected MasterInstall_14_04 trying to call EntertainmentSuite_14_04, which doesn't exist.

--- MasterInstall_14_04.sh
+++ MasterInstall_14_04.sh
@@ -4,5 +4,5 @@ bash AutoInstall/SystemTools.sh
 bash AutoInstall/CreativeSuite.sh
 bash AutoInstall/WorkSuite.sh
 bash AutoInstall/ProgrammingSuite_14_04.sh
-bash AutoInstall/EntertainmentSuite_14_04.sh
+bash AutoInstall/EntertainmentSuite.sh
 bash AutoInstall/GamingSuite_14_04.sh

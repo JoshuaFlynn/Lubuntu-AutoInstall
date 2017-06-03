e8c7981b93a7a7ebf5501f994da2c4d5758060fb
Joshua Flynn <flynn4anything@hotmail.com>
2017-06-03 18:34:11 +0100

    Update Security_14_04.sh
    Adding a firewall port block for the Samba service port. There isn't evidence to indicate Lubuntu uses Samba (there is no Samba config file nor folder under /etc/). Users can remove it from gufw after if they don't want it. It is however there to aid protection against the WannaCry script.

--- AutoInstall/Security_14_04.sh
+++ AutoInstall/Security_14_04.sh
@@ -3,6 +3,13 @@
 #We try to enable the firewall on our first pass
 ufw enable
 
+#Protection against the Samba exploit. Hopefully.
+ufw deny in 445
+ufw deny out 445
+
+#Reload the firewall so the new rules take effect
+ufw reload
+
 #repository requirement for dnscrypt if we're using 14.04
 add-apt-repository ppa:anton+/dnscrypt
 apt-get update

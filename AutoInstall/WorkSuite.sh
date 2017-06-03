e8ad7dae5786a7c5257ef573b933e15b5aa05af0
Joshua Flynn <flynn4anything@hotmail.com>
2017-06-03 15:23:48 +0100

    Update WorkSuite.sh
    Added feature upgrade to auto-install language support packs for LibreOffice (ideally making sure autocorrect works on languages other than ego-centric American).

--- AutoInstall/WorkSuite.sh
+++ AutoInstall/WorkSuite.sh
@@ -2,6 +2,9 @@ apt-get update
 apt-get install -y libreoffice
 apt-get install -y evolution
 
+#This checks for language support that libreoffice etc might have missed
+apt-get install $(check-language-support)
+
 apt-get remove -y sylpheed
 apt-get remove -y gnumeric
 apt-get remove -y abiword

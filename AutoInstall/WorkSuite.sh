519e39245629c3fcb6c38ad0f8c2ee5d40ae1968
Joshua Flynn <flynn4anything@hotmail.com>
2017-08-18 19:35:55 +0100

    Update WorkSuite.sh

--- AutoInstall/WorkSuite.sh
+++ AutoInstall/WorkSuite.sh
@@ -3,8 +3,12 @@ apt-get install -y libreoffice
 apt-get install -y evolution
 
 #This checks for language support that libreoffice etc might have missed
-apt-get install $(check-language-support)
+apt-get install -y $(check-language-support)
 
+#We add in git itself! Woo!
+apt-get install -y git
+
+#These programs are not needed, largely due to the suite we install
 apt-get remove -y sylpheed
 apt-get remove -y gnumeric
 apt-get remove -y abiword
